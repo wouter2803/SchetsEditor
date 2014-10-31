edf6495666afbe229326d498cfa1d388cb9465c7
wouter2803 <wouter2803@users.noreply.github.com>
2014-10-31 11:47:26 +0100

    SchetsEditor
    SchetsEditor

---
+++ Hoofdscherm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO; //zelf
+
+namespace SchetsEditor
+{
+    public class Hoofdscherm : Form
+    {
+        MenuStrip menuStrip;
+        Schets schets;
+
+        public Hoofdscherm(Schets sch)
+        {   this.ClientSize = new Size(800, 600);
+            menuStrip = new MenuStrip();
+            this.Controls.Add(menuStrip);
+            this.maakFileMenu();
+            this.maakHelpMenu();
+            this.Text = "Schets editor";
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = menuStrip;
+
+            this.schets = sch;
+        }
+        private void maakFileMenu()
+        {   ToolStripDropDownItem menu;
+            menu = new ToolStripMenuItem("File");
+            menu.DropDownItems.Add("Nieuw", null, this.nieuw);
+            menu.DropDownItems.Add("Openen", null, this.openen);
+            menu.DropDownItems.Add("Exit", null, this.afsluiten);
+            menuStrip.Items.Add(menu);
+        }
+        private void maakHelpMenu()
+        {   ToolStripDropDownItem menu;
+            menu = new ToolStripMenuItem("Help");
+            menu.DropDownItems.Add("Over \"Schets\"", null, this.about);
+            menuStrip.Items.Add(menu);
+        }
+        private void about(object o, EventArgs ea)
+        {   MessageBox.Show("Schets versie 1.0\n(c) UU Informatica 2010"
+                           , "Over \"Schets\""
+                           , MessageBoxButtons.OK
+                           , MessageBoxIcon.Information
+                           );
+        }
+
+        private void nieuw(object sender, EventArgs e)
+        {   SchetsWin s = new SchetsWin();
+            s.MdiParent = this;
+            s.Show();
+        }
+
+        //
+        private void openen(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Jpeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            open.Title = "Open a File";
+            
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                SchetsWin s = new SchetsWin();
+                s.MdiParent = this;
+                Bitmap bm = new Bitmap(open.FileName);
+                s.setbitmap = bm;
+                s.Show();
+            }
+
+
+        }
+
+        private void afsluiten(object sender, EventArgs e)
+        {   this.Close();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Hoofdscherm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Hoofdscherm";
+            this.ResumeLayout(false);
+
+        }
+
+        
+    }
+}
---
+++ Objecten.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Collections.Generic;
+
+namespace SchetsEditor
+{
+
+    public abstract class MaakObject
+    {
+        public Pen pen;
+        public Brush Kwast;
+        public Rectangle Rechthoek;
+
+        public abstract bool geklikt();
+    }
+
+    public class MaakCirkel : MaakObject
+    {
+        public MaakCirkel(Pen p, Rectangle rechthoek)
+        {
+            this.pen = p;
+            this.Rechthoek = rechthoek;
+        }
+
+        public override bool geklikt()
+        {
+            return false;
+        }
+    }
+
+    public class MaakRondje : MaakObject
+    {
+        public MaakRondje(Brush kwast, Rectangle rechthoek)
+        {
+            this.Kwast = kwast;
+            this.Rechthoek = rechthoek;
+        }
+
+        public override bool geklikt()
+        {
+            return false;
+        }
+    }
+}
